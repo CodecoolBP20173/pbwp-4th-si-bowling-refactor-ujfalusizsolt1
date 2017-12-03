0d58e39adc85b44b8f531f0704f21867cbc06407
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2017-12-03 22:57:20 +0100

    deleted unnecessary else and condition

--- bowling.py
+++ bowling.py
@@ -8,10 +8,11 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
+
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == 'x':
                 result += get_value(game[i + 1])
                 if game[i + 2] == '/':
                     result += 10 - get_value(game[i + 1])
@@ -20,10 +21,9 @@ def score(game):
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half:
-            in_first_half = False
-        else:
             in_first_half = True
+        else:
+            in_first_half = False
         if game[i].lower() == 'x':
             in_first_half = True
             frame += 1
