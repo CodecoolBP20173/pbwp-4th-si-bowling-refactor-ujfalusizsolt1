970ecfd569a1c41b922fc40b507a2fdcd35eea08
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2017-12-03 21:16:53 +0100

    cleared up get_value

--- bowling.py
+++ bowling.py
@@ -2,6 +2,7 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
+
     for i in range(len(game)):
         if game[i] == '/':
             result += 10 - last
@@ -9,19 +10,19 @@ def score(game):
             result += get_value(game[i])
         # if not in_first_half:
             # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                result += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
+                    result += get_value(game[i + 2])
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
@@ -30,14 +31,12 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    if char in numbers:
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char.lower() == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
